475dc74391f0a526a5901f39ec194de646267b80
Pedro Santana <pedro.santana9905@gmail.com>
2025-04-09 14:24:47 -0300

    Add files via upload

---
+++ 002/js/script.js
@@ -0,0 +1,8 @@
+//subtração
+let num1 = parseInt(prompt("Digite um número: ")) 
+let num2 = parseInt(prompt("Digite o outro número: ")) 
+
+let soma = num1 - num2
+
+alert("O resultado da soma desses números é "+ soma)
+
